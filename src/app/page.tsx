86a53ca20f8ffb2ad4e69a3bb26dbe84add2ab01
Ruslan Gonzalez <ruslanguns@gmail.com>
2024-11-03 12:51:11 +0100

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <div>
+          <div className="card bg-base-100 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">99€</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary"
+                  href="https://buy.stripe.com/test_00g6ozdo259362I144"
+                >
+                  Buy Now
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Terms and conditions
+        </a>{" "}
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Private Policy
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Us
+        </a>
+      </footer>
+    </div>
+  );
+}
